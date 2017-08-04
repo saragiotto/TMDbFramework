67321d10b419fc155bbbf27747f43ad6e5ec00f5
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2017-08-03 21:22:59 -0300

    Install and Test

---
+++ Example/Pods/Target Support Files/Pods-TMDbFramework_Example/Pods-TMDbFramework_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TMDbFramework_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TMDbFramework_Example
+@end
---
+++ Example/Pods/Target Support Files/TMDbFramework/TMDbFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TMDbFramework : NSObject
+@end
+@implementation PodsDummy_TMDbFramework
+@end
