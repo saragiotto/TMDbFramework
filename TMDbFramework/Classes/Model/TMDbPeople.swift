809aed6a2b5824d19754926db8a1cefdb4e73320
Leonardo Saragiotto <leonardo.saragiotto@gmail.com>
2018-02-08 23:31:39 -0200

    New model People

--- TMDbFramework/Classes/Model/TMDbPeople.swift
+++ TMDbFramework/Classes/Model/TMDbPeople.swift
@@ -9,5 +9,33 @@ import Foundation
 import SwiftyJSON
 
 open class TMDbPeople {
+    public let adult:Bool?
+    public let biography:String?
+    public let deathday:String?
+    public let gender:Int?
+    public let homepage:String?
+    public let id:Int?
+    public let imdbId:String?
+    public let name:String?
+    public let placeOfBirth:String?
+    public let popularity:Int?
+    public let profilePath:String?
     
+    init(data:JSON) {
+        adult = data["adult"].bool
+        id = data["id"].int
+        name = data["name"].string
+        profilePath = data["profile_path"].string
+        popularity = data["popularity"].double
+    }
+    
+    func populateDetail(data: JSON) {
+        biography = data["biography"].string
+        birthday = data["birthday"].string
+        deathday = data["deathday"].string
+        gender = data["gender"].int
+        homepage = data["homepage"].string
+        imdbId = data["imdb_id"].string
+        placeOfBirth = data["place_of_birth"].string
+    }
 }
